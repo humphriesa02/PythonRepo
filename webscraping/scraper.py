6f615ed8a3393249de6deb0ff14a6a09b393e2c4
Hero <humphriesa02@gmail.com>
2021-07-27 17:45:40 -0700

    Added Steam Webscrape

---
+++ webscraping/scraper.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from datetime import date
+import requests
+
+html_text = requests.get('https://store.steampowered.com/specials/#p=0&tab=TopSellers').text
+
+soup = BeautifulSoup(html_text, 'lxml')
+
+list = soup.find('div', id = 'TopSellersRows')
+#discountPercentage = listElements.find_all('div', class_ = 'col search_discount responsive_secondrow')
+listElements = list.find_all('a')
+
+print('Steam Top Sellers Discounts for: ' + str(date.today()))
+for elements in listElements:
+    #listImg = elements.find('div','col search_capsule')
+    listName = elements.find('div', class_ = 'tab_item_name').text
+    listDiscount = elements.find('div', class_ = 'discount_pct').text.replace(' ', '')
+    origPrice = elements.find('div', class_ = 'discount_original_price').text
+    discountPrice = elements.find('div', class_ = 'discount_final_price').text
+    print('Name: ' +listName)
+    print('Discount: ' +listDiscount)
+    print('Original Price: ' +origPrice)
+    print('Discount Price: ' +discountPrice)
+    print('')
+    
